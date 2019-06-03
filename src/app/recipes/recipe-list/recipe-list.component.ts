f9a53a61a6ed8fc7809b99f89b02783fbb73c42a
amitkumar11240 <amit.kumar@cloudanalogy.com>
2019-06-03 19:44:03 +0530

    Recipe List and Shopping List

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -11,5 +11,9 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test recipe', 'https://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/1101-couscous-chicken-ck.jpg?itok=7geW7sC_')
+  ];
 
 }
